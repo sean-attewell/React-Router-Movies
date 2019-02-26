70cebc9375a34577c326f6b173d3c803cad2504b
sean-attewell <sean.attewell@gmail.com>
2019-02-26 13:25:12 +0000

    added NavLink to SavedList

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -12,9 +13,15 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`}>
+          <NavLink 
+          to={`/movies/${movie.id}`}
+          activeStyle={{
+            fontWeight: 'bold',
+            color: 'red'
+          }}
+          >
             <span className="saved-movie">{movie.title}</span>
-          </Link>
+          </NavLink>
         ))}
         <Link exact to={`/`} className="home-button">Home</Link>
       </div>
